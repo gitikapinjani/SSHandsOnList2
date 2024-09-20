9e1c5e397dfc5f5fa40a69fbabd8dc918da0e094
gitikapinjani <gitika.raipur123@gmail.com>
2024-09-20 23:37:59 +0530

    6th commit

--- 19c.c
+++ 19c.c
@@ -1,2 +1,13 @@
-mknodat(AT_FDCWD, "myfifo", S_IFIFO|0666) = 0
-mknodat(AT_FDCWD, "myfifo", S_IFIFO|0666) = -1
+
+/*
+command: strace mknod myfifo p
+output: mknodat(AT_FDCWD, "myfifo", S_IFIFO|0666)
+
+command: strace mkfifo myfifo
+outputL: mknodat(AT_FDCWD, "myfifo", S_IFIFO|0666)
+
+both mknod mkfifo ultimatelt rely on mknod() system call to create FIFO.
+
+mkfifo is better because it is specifically designed for creating FIFO's and abstract system call detail from the user.
+
+*/
